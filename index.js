e5df4578215e8a11946370ac1394615bf8675b7d
quincy mwangi <mwangiquincy77@gmail.com>
2024-07-13 15:53:14 +0300

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1=31;
+const num2=2;
+let multiply=num1 * num2;
+console.log(multiply);
+const num3=3;
+const num4=1;
+let random=num3*num4;
+console.log(random);
+const num5=24;
+const num6=5;
+let mod=num5%num6;
+console.log(mod);
 
-// Write your code here
+let max=Math.max(5,10,15,20);
+console.log(max);
